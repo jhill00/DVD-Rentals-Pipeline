d0685373ac5642011bbd53ffc310331097dc83b0
James H <hill0596@gmail.com>
2023-10-12 23:11:44 -0700

    organized directories

--- dimensional_modeling/dimensions/insert_dim_data.sql
+++ dimensional_modeling/dimensions/insert_dim_data.sql
@@ -1,5 +1,3 @@
--- insert data to dimension tables
-
 INSERT INTO dim_date
 (date_key, date, year, quarter, month, day, weekend) 
 SELECT
@@ -98,27 +96,4 @@ FROM
 	film_actor as fa
 INNER JOIN
 	actor as a ON a.actor_id = fa.actor_id
-;
-
--- insert data to fact table
-
-INSERT INTO fact_sales
-(date_key, store_id, customer_id, film_id, film_actor_id, sales)
-SELECT
-	DISTINCT(TO_CHAR(p.payment_date, 'yyyymmdd')::INTEGER) as date_key,
-	s.store_id,
-	p.customer_id,
-	i.film_id,
-	CONCAT(fa.film_id, '_', fa.actor_id) as film_actor_id,
-	p.amount as sales
-FROM
-	payment as p
-INNER JOIN
-	rental as r ON r.rental_id = p.rental_id
-INNER JOIN
-	inventory as i ON i.inventory_id = r.inventory_id
-INNER JOIN
-	film_actor as fa ON fa.film_id = i.film_id
-INNER JOIN
-	staff as s ON s.staff_id = p.staff_id
 ;
---
+++ dimensional_modeling/facts/ceate_facts.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS fact_sales
+(
+	date_key INTEGER REFERENCES dim_date(date_key),
+	store_id INTEGER REFERENCES dim_store(store_id),
+	customer_id SMALLINT REFERENCES dim_customer(customer_id),
+	film_id INTEGER REFERENCES dim_film(film_id),
+	film_actor_id VARCHAR(50) REFERENCES dim_actor(film_actor_id),
+	sales REAL
+);
